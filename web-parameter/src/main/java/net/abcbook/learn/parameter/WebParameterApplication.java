0e1d27ac05b413df7ea20d1d0f1206183338249e
summer <lixian13149999@163.com>
2018-06-19 14:59:58 +0800

    Initial commit

---
+++ web-parameter/src/main/java/net/abcbook/learn/parameter/WebParameterApplication.java
@@ -0,0 +1,12 @@
+package net.abcbook.learn.parameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebParameterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebParameterApplication.class, args);
+    }
+}
